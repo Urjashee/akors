dee88c6a6fa0fbb189f6d96e0b3a64d29ee7d13d
Urjashee <urja@simpalm.com>
2026-02-13 14:15:27 +0530

    aloowed hosts

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "3.236.244.132"
+]
 
 
 # Application definition
